2268a4600dbd44598cf3a1a0fd92be75446c6dd5
Daniel McKnight <daniel@neon.ai>
2022-07-19 13:18:06 -0700

    Fix skill_id typo in skill loader unit tests

--- test/unittests/test_skill_loading.py
+++ test/unittests/test_skill_loading.py
@@ -10,7 +10,7 @@ from mycroft.skills.skill_loader import PluginSkillLoader, SkillLoader
 class TestSkillLoading(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.skill_id = "ovos-skill-homescreen.OpenVoiceOS"
+        self.skill_id = "skill-homescreen-lite.openvoiceos"
         self.path = dirname(dirname(dirname(__file__)))
 
     def test_from_class(self):
