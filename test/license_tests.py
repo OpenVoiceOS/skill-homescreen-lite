a3dc235c4f482eee50c63132adf3cda3527744f1
Daniel McKnight <daniel@neon.ai>
2022-07-19 13:07:56 -0700

    Update license overrides

--- test/license_tests.py
+++ test/license_tests.py
@@ -10,7 +10,10 @@ license_overrides = {
     'yt-dlp': "Unlicense",
     'pyxdg': 'GPL-2.0',
     'ptyprocess': 'ISC license',
-    'psutil': 'BSD3'
+    'psutil': 'BSD3',
+    "python-dateutil": "Apache-2.0",
+    "pyparsing": "MIT",
+    "exceptiongroup": "MIT"
 }
 # explicitly allow these packages that would fail otherwise
 whitelist = []
