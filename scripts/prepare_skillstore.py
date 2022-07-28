690dc237cb0808fe01448364dec2c9fa4889ad9c
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2022-07-28 10:19:42 -0700

    Update missed URL in `prepare_skillstore.py` (#2)
    Co-authored-by: Daniel McKnight <daniel@neon.ai>

--- scripts/prepare_skillstore.py
+++ scripts/prepare_skillstore.py
@@ -8,7 +8,7 @@ from ovos_utils.bracket_expansion import expand_parentheses, expand_options
 
 
 branch = "dev"
-repo = "ovos-skill-homescreen"
+repo = "skill-homescreen-lite"
 author = "OpenVoiceOS"
 
 url = f"https://github.com/{author}/{repo}@{branch}"
